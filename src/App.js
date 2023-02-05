313218f3cc1d95d0ef4118c639f795b7a7cf6553
seona.bang <sit.or.seona@gmail.com>
2023-02-05 20:16:53 +0900

    design: globalStyles, font, theme 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./style/globalStyles";
+import theme from "./style/theme";
 
 function App() {
   return (
     <div className="App">
-      <ThemeProvider></ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/style/theme.jsx
@@ -0,0 +1,15 @@
+const palette = {
+  main: "#BB2649",
+  white: "#ffffff",
+  black: "#000000",
+  darkGray: "#767676",
+  mediumGray: "#C4C4C4",
+  lightGray: "#DBDBDB",
+  whitishGray: "#F2F2F2",
+};
+
+const theme = {
+  palette,
+};
+
+export default theme;

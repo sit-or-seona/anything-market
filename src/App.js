5813c091ac014a0146b9e06cf60308d38d7c6e4c
seona.bang <sit.or.seona@gmail.com>
2023-02-05 19:51:25 +0900

    chore: 초기 세팅

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "styled-components";
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeProvider></ThemeProvider>
+    </div>
+  );
+}
+
+export default App;

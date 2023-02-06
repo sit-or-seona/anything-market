14491e05ef5198c2eefe18a0fc4efc04e2272962
seona.bang <sit.or.seona@gmail.com>
2023-02-06 23:50:52 +0900

    chore: 페이지 폴더 세팅 #1

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export { CartPage } from "./CartPage";
+export { HomePage } from "./HomePage";
+export { LoginPage } from "./LoginPage";
+export { MyPage } from "./MyPage";
+export { NotFoundPage } from "./NotFoundPage";
+export { OrderPage } from "./OrderPage";
+export { ProductPage } from "./ProductPage";
+export { SignupPage } from "./SignupPage";

9775933753b64d2f681c510c044ec7a73edc095a
seona.bang <sit.or.seona@gmail.com>
2023-02-14 15:20:07 +0900

    style: 공통 컴포넌트 - input UI 스타일 수정 #8

--- src/components/common/Input/index.jsx
+++ src/components/common/Input/index.jsx
@@ -18,21 +18,25 @@ export const Input = styled.input`
   }
 `;
 
-export const Label = styled.label`
-  display: block;
-  font-size: 16px;
-  color: ${({ theme }) => theme.palette.darkGray};
-`;
+export const InputBox = styled.div`
+  input {
+    margin: 10px 0;
+    padding: 16px 16px;
+    border: 1px solid ${({ theme }) => theme.palette.mediumGray};
+    border-radius: 5px;
+    color: ${({ theme }) => theme.palette.black};
+    line-height: 20px;
+    font-size: 16px;
+    outline: none;
 
-export const InputBox = styled(Input)`
-  height: auto;
-  margin: 10px 0;
-  padding: 16px 16px;
-  border: 1px solid ${({ theme }) => theme.palette.mediumGray};
-  border-radius: 5px;
-  color: ${({ theme }) => theme.palette.black};
+    &:focus {
+      border: 1px solid ${({ theme }) => theme.palette.main};
+    }
+  }
 
-  &:focus {
-    border: 1px solid ${({ theme }) => theme.palette.main};
+  label {
+    display: block;
+    font-size: 16px;
+    color: ${({ theme }) => theme.palette.darkGray};
   }
 `;
